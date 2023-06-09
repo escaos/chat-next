c1decf8a8efddb89f8ae3fc5efc1d99e10b219c0
Edison Sanchez <edison@edisonsanchez.com>
2023-06-09 18:42:02 -0400

    Initial Version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+  purge: [
+    // Add paths to your components here
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: false,
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,12 +13,30 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'primary': 'var(--primary)',
+        'primary-grad-light-mid': 'var(--primary-grad-light-mid)',
+        'primary-grad-light-end': 'var(--primary-grad-light-end)',
+        'primary-grad-dark-mid': 'var(--primary-grad-dark-mid)',
+        'primary-grad-dark-end': 'var(--primary-grad-dark-end)',
+        'secondary': 'var(--secondary)',
+        'secondary-grad-light-mid': 'var(--secondary-grad-light-mid)',
+        'secondary-grad-light-end': 'var(--secondary-grad-light-end)',
+        'secondary-grad-dark-mid': 'var(--secondary-grad-dark-mid)',
+        'secondary-grad-dark-end': 'var(--secondary-grad-dark-end)',
+        'button-primary': 'var(--button-primary)',
+        'button-secondary': 'var(--button-secondary)',
+        'button-text': 'var(--button-text)',
+        'link-text': 'var(--link-text)',
+        'text': 'var(--text)',
+        'placeholder': 'var(--placeholder)',
+        'background': 'var(--background)',
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-button-light': 'linear-gradient(to right, #1E88E5, #005cb2, #004a8c)',
+        'gradient-button-dark': 'linear-gradient(to right, #005cb2, #3377b8, #004a8c)',
       },
     },
   },
   plugins: [],
-}
+};
